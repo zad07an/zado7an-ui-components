b1bd5db13bb105a9830b58f035bb283782fd2f70
Dev <zad07an@gmail.com>
2025-02-13 23:12:45 +0400

    fixing error 'Cannot find module 'zado7an-ui-components' or its corresponding type declarations.ts(2307)' after installation

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["index.ts"],
-  format: ["esm", "cjs"],
-  dts: true,
-  tsconfig: "./tsconfig.app.json", // Explicitly point to your tsconfig
+  format: ["esm", "cjs"], // Output both ESM and CJS formats
+  dts: true, // Generate type declaration files
+  tsconfig: "./tsconfig.app.json", // Explicitly point to your tsconfig if needed
 });
